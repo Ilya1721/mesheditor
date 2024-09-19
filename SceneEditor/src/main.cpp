4a25d793e1928723ec8e61b895c60fa47f326c4a
ilya1721 <ilyavusnya@gmail.com>
2024-09-19 20:38:36 +0300

    Fixed main

--- SceneEditor/src/main.cpp
+++ SceneEditor/src/main.cpp
@@ -4,7 +4,7 @@ using namespace RenderSystem;
 
 int main()
 {
-	Window mainWindow(1200, 628, R"(D:\3DModels\STL\Cube\Cube.stl)");
+	Window mainWindow(1200, 628, R"(C:\MyPersonalSpace\Dev\3DModels\STL\Cube\Cube.stl)");
 	mainWindow.render();
 
 	return 0;
